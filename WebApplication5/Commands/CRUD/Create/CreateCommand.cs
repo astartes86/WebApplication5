82436dc38c502c2201efbb4d3cba2985400be10f
User <User@DESKTOP-0FQVJVH>
2024-10-03 03:28:53 +0500

    первые тесты, пытаюсь вникнуть в их смысл

--- WebApplication5/Commands/CRUD/Create/CreateCommand.cs
+++ WebApplication5/Commands/CRUD/Create/CreateCommand.cs
@@ -5,6 +5,7 @@ namespace WebApplication5.Commands.CRUD.Create
     public class CreateCommand<TEntity> : IRequest<TEntity>
 
     {
+        public CreateCommand()
         public TEntity Entity { get; set; }
 
         public CreateCommand(TEntity entity)
